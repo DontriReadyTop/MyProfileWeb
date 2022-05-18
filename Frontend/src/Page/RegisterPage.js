9388b245c091c5d1db5061938e1ed6916802b813
ReadyTop <kamjamnong.t@gmail.com>
2022-05-19 00:04:03 +0700

    fix package

--- Frontend/src/Page/RegisterPage.js
+++ Frontend/src/Page/RegisterPage.js
@@ -2,8 +2,49 @@ import './RegisterPage.css';
 import { Link } from "react-router-dom";
 import { BiUserCircle, BiLock, BiLockAlt, BiPhone } from "react-icons/bi";
 import { AiOutlineMail } from "react-icons/ai";
+import { useState } from 'react';
 
 function RegisterPage() {
+
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [phone, setPhone] = useState(null);
+
+  const checkEmpty = (temp) => {
+    if (temp == "") {
+      return null;
+    }else{
+      return temp;
+    }
+  }
+
+  const handleUsernameChange = (event) => {
+    setUsername(checkEmpty(event.target.value));
+  }
+  const handlePasswordChange = (event) => {
+    setPassword(checkEmpty(event.target.value));
+  }
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(checkEmpty(event.target.value));
+  }
+  const handleEmailChange = (event) => {
+    setEmail(checkEmpty(event.target.value));
+  }
+  const handlePhoneChange = (event) => {
+    setPhone(checkEmpty(event.target.value));
+  }
+
+  const submitRegister = async () => {
+    console.log(username);
+    console.log(password);
+    console.log(confirmPassword);
+    console.log(email);
+    console.log(phone);
+
+    
+  }
 
   return (
     <>
@@ -14,26 +55,26 @@ function RegisterPage() {
             <form>
               <div className="form-group">
                 <label className="icon-register" htmlFor="Username"><BiUserCircle size={21} /></label>
-                <input className="input-register" type="text" name="Username" id="Username" placeholder="Your Username" />
+                <input className="input-register" type="text" name="Username" id="Username" placeholder="Your Username" value={username} onChange={handleUsernameChange} />
               </div>
               <div className="form-group">
                 <label className="icon-register" htmlFor="Email"><AiOutlineMail size={21} /></label>
-                <input className="input-register" type="text" name="Email" id="Email" placeholder="Your Email" />
+                <input className="input-register" type="text" name="Email" id="Email" placeholder="Your Email" value={email} onChange={handleEmailChange} />
               </div>
               <div className="form-group">
                 <label className="icon-register" htmlFor="Phone"><BiPhone size={21} /></label>
-                <input className="input-register" type="text" name="Phone" id="Phone" placeholder="Your Phone" />
+                <input className="input-register" type="text" name="Phone" id="Phone" placeholder="Your Phone" value={phone} onChange={handlePhoneChange} />
               </div>
               <div className="form-group">
                 <label className="icon-register" htmlFor="Password"><BiLock size={21} /></label>
-                <input className="input-register" type="password" name="Password" id="Password" placeholder="Password" />
+                <input className="input-register" type="password" name="Password" id="Password" placeholder="Password" value={password} onChange={handlePasswordChange} />
               </div>
               <div className="form-group">
                 <label className="icon-register" htmlFor="ConfirmPassword"><BiLockAlt size={21} /></label>
-                <input className="input-register" type="password" name="ConfirmPassword" id="ConfirmPassword" placeholder="Repeat your password" />
+                <input className="input-register" type="password" name="ConfirmPassword" id="ConfirmPassword" placeholder="Repeat your password" value={confirmPassword} onChange={handleConfirmPasswordChange} />
               </div>
-              <div className="btn-register">
-                <p><Link to="/login">Register</Link></p>
+              <div className="btn-register" onClick={submitRegister}>
+                <p>Register</p>
               </div>
             </form>
           </div>
